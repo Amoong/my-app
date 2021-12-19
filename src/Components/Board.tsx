8ba811aa820e7d89f637befcbb784557c4bc6424
Amoong <moo1323@naver.com>
2021-12-19 11:25:52 +0900

    💄 Add board and card styling

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -3,11 +3,13 @@ import styled from "styled-components";
 import DragableCard from "./DragabbleCard";
 
 const Wrapper = styled.div`
-  background-color: ${props => props.theme.boardColor};
-  padding: 20px 10px;
+  background-color: ${(props) => props.theme.boardColor};
+  padding: 10px 0;
   padding-top: 10px;
   border-radius: 5px;
   min-height: 200px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h1`
@@ -16,7 +18,19 @@ const Title = styled.h1`
   font-weight: bold;
   text-align: center;
   margin-bottom: 10px;
-  upper
+`;
+
+interface IArea {
+  isDraggingFromThis: boolean;
+  isDraggingOver: boolean;
+}
+
+const Area = styled.div<IArea>`
+  background: ${(props) =>
+    props.isDraggingOver ? "pink" : props.isDraggingFromThis ? "gold" : "transparent"};
+  flex-grow: 1;
+  transition: background-color 0.3s ease-in-out;
+  padding: 10px;
 `;
 
 interface IBoardProps {
@@ -26,17 +40,24 @@ interface IBoardProps {
 
 function Board({ toDos, boardId }: IBoardProps) {
   return (
-    <Droppable droppableId={boardId}>
-      {magic => (
-        <Wrapper ref={magic.innerRef} {...magic.droppableProps}>
-          <Title>{boardId}</Title>
-          {toDos.map((toDo, i) => (
-            <DragableCard key={toDo} toDo={toDo} index={i} />
-          ))}
-          {magic.placeholder}
-        </Wrapper>
-      )}
-    </Droppable>
+    <Wrapper>
+      <Title>{boardId}</Title>
+      <Droppable droppableId={boardId}>
+        {(magic, info) => (
+          <Area
+            isDraggingOver={info.isDraggingOver}
+            isDraggingFromThis={!!info.draggingFromThisWith}
+            ref={magic.innerRef}
+            {...magic.droppableProps}
+          >
+            {toDos.map((toDo, i) => (
+              <DragableCard key={toDo} toDo={toDo} index={i} />
+            ))}
+            {magic.placeholder}
+          </Area>
+        )}
+      </Droppable>
+    </Wrapper>
   );
 }
 
--- src/Components/DragabbleCard.tsx
+++ src/Components/DragabbleCard.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   padding: 10px 10px;
-  background-color: ${props => props.theme.cardColor};
+  background-color: ${(props) => (props.isDragging ? "skyblue" : props.theme.cardColor)};
   border-radius: 5px;
   margin-bottom: 5px;
+  box-shadow: ${(props) => (props.isDragging ? "2px 2px 2px 1px rgba(0, 0, 0, 0.2)" : "none")};
 `;
 
 interface IDragableCardProps {
@@ -15,11 +16,15 @@ interface IDragableCardProps {
 }
 
 function DragableCard({ toDo, index }: IDragableCardProps) {
-  console.log(toDo, "had been rendered");
   return (
     <Draggable key={toDo} draggableId={toDo} index={index}>
-      {magic => (
-        <Card ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
+      {(magic, info) => (
+        <Card
+          isDragging={info.isDragging}
+          ref={magic.innerRef}
+          {...magic.draggableProps}
+          {...magic.dragHandleProps}
+        >
           {toDo}
         </Card>
       )}
