0c8cc24900aad4d37814b68904352db0d410464d
MuYoungJeong <moo1323@naver.com>
2021-12-19 19:03:01 +0900

    Refactor variants

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
@@ -16,11 +16,15 @@ const Box = styled.div`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box />
-      <motion.div></motion.div>
+      <Box variants={myVars} initial="start" animate="end" />
     </Wrapper>
   );
 }
