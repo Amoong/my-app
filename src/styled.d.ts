c11684fbf8786dd65729dcddb3ade9c2a5cf10a0
MuYoungJeong <moo1323@naver.com>
2021-12-19 16:25:37 +0900

    Start framer-motion

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,13 +1,5 @@
-// import original module declarations
 import "styled-components";
 
-// and extend them!
 declare module "styled-components" {
-  export interface DefaultTheme {
-    textColor: string;
-    bgColor: string;
-    accentColor: string;
-    boardColor: string;
-    cardColor: string;
-  }
+  export interface DefaultTheme {}
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,3 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "#3880F3",
-  textColor: "black",
-  accentColor: "#2ecc71",
-  boardColor: "lightgray",
-  cardColor: "white",
-};
+export const darkTheme: DefaultTheme = {};
