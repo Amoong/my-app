9f24a339d3b6b8b1f1b6b6b0878d286e49e0a7f6
MuYoungJeong <moo1323@naver.com>
2021-12-20 21:58:19 +0900

    Svg animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform, useViewportScroll } from "framer-motion";
-import { useEffect } from "react";
+import { motion } from "framer-motion";
 const Wrapper = styled(motion.div)`
-  height: 200vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -10,36 +9,37 @@ const Wrapper = styled(motion.div)`
   background: linear-gradient(135deg, #1b926a, #25725d);
 `;
 
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 30px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Svg = styled.svg`
+  width: 240px;
+  height: 240px;
+  path {
+    stroke: white;
+    stroke-width: 0.2;
+  }
 `;
 
+const svg = {
+  start: { pathLength: 0, fill: "rgba(255,255,255,0)" },
+  end: { pathLength: 1, fill: "rgba(255,255,255,1)" },
+};
+
 function App() {
-  const x = useMotionValue(0);
-
-  const rotateZ = useTransform(x, [-800, 800], [-3600, 3600]);
-  const gradient = useTransform(
-    x,
-    [-800, 800],
-    ["linear-gradient(135deg, #6668ca, #253d72)", "linear-gradient(135deg, #1b926a, #25725d)"],
-  );
-  const { scrollY, scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-
-  useEffect(() => {
-    scrollY.onChange(() => {});
-  }, [scrollY, scrollYProgress]);
-
   return (
-    <Wrapper style={{ background: gradient }}>
-      <button onClick={() => x.set(200)}>click me</button>
-      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin></Box>
+    <Wrapper>
+      <Svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <motion.path
+          variants={svg}
+          initial="start"
+          animate="end"
+          transition={{
+            default: { duration: 5 },
+            fill: { duration: 2, delay: 3 },
+          }}
+          // stroke="white"
+          // strokeWidth="0.2"
+          d="M17.516 3c2.382 0 4.487 1.564 4.487 4.712 0 4.963-6.528 8.297-10.003 11.935-3.475-3.638-10.002-6.971-10.002-11.934 0-3.055 2.008-4.713 4.487-4.713 3.18 0 4.846 3.644 5.515 5.312.667-1.666 2.333-5.312 5.516-5.312zm0-2c-2.174 0-4.346 1.062-5.516 3.419-1.17-2.357-3.342-3.419-5.515-3.419-3.403 0-6.484 2.39-6.484 6.689 0 7.27 9.903 10.938 11.999 15.311 2.096-4.373 12-8.041 12-15.311 0-4.586-3.414-6.689-6.484-6.689z"
+        />
+      </Svg>
     </Wrapper>
   );
 }
