cefbd76a4c0e39b7e306530f4cf48be307538ff6
MuYoungJeong <moo1323@naver.com>
2021-12-22 17:46:14 +0900

    AnimatePresence

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
@@ -9,37 +11,39 @@ const Wrapper = styled(motion.div)`
   background: linear-gradient(135deg, #1b926a, #25725d);
 `;
 
-const Svg = styled.svg`
-  width: 240px;
-  height: 240px;
-  path {
-    stroke: white;
-    stroke-width: 0.2;
-  }
+const Box = styled(motion.div)`
+  width: 300px;
+  height: 300px;
+  background-color: white;
+  position: absolute;
+  border-radius: 10px;
+  box-shadow: 2px 2px 5px 5px rgba(0, 0, 0, 0.2);
+  top: 100px;
+  left: auto;
+  bottom: auto;
+  right: auto;
 `;
 
-const svg = {
-  start: { pathLength: 0, fill: "rgba(255,255,255,0)" },
-  end: { pathLength: 1, fill: "rgba(255,255,255,1)" },
-};
-
 function App() {
+  const [showing, setShowing] = useState(false);
+  const toggleShowing = () => setShowing(prev => !prev);
+
+  const boxVariants = {
+    initial: { opacity: 0, scale: 0 },
+    visible: { opacity: 1, scale: 1, rotateZ: 360 },
+    leaving: { opacity: 0, scale: 0, y: 50 },
+  };
+
   return (
     <Wrapper>
-      <Svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-        <motion.path
-          variants={svg}
-          initial="start"
-          animate="end"
-          transition={{
-            default: { duration: 5 },
-            fill: { duration: 2, delay: 3 },
-          }}
-          // stroke="white"
-          // strokeWidth="0.2"
-          d="M17.516 3c2.382 0 4.487 1.564 4.487 4.712 0 4.963-6.528 8.297-10.003 11.935-3.475-3.638-10.002-6.971-10.002-11.934 0-3.055 2.008-4.713 4.487-4.713 3.18 0 4.846 3.644 5.515 5.312.667-1.666 2.333-5.312 5.516-5.312zm0-2c-2.174 0-4.346 1.062-5.516 3.419-1.17-2.357-3.342-3.419-5.515-3.419-3.403 0-6.484 2.39-6.484 6.689 0 7.27 9.903 10.938 11.999 15.311 2.096-4.373 12-8.041 12-15.311 0-4.586-3.414-6.689-6.484-6.689z"
-        />
-      </Svg>
+      <button onClick={toggleShowing}>Click</button>
+      <AnimatePresence>
+        {showing ? (
+          <Box variants={boxVariants} initial="initial" animate="visible" exit="leaving">
+            adsfads
+          </Box>
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
