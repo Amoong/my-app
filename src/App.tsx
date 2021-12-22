a28d58ae31009bb18861208b0e320c931cb16d27
MuYoungJeong <moo1323@naver.com>
2021-12-22 19:13:07 +0900

    Slider part Two

--- src/App.tsx
+++ src/App.tsx
@@ -8,42 +8,44 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   background: linear-gradient(135deg, #1b926a, #25725d);
 `;
 
 const Box = styled(motion.div)`
-  width: 300px;
-  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 100px;
   background-color: white;
-  position: absolute;
   border-radius: 10px;
   box-shadow: 2px 2px 5px 5px rgba(0, 0, 0, 0.2);
+  position: absolute;
   top: 100px;
-  left: auto;
-  bottom: auto;
-  right: auto;
 `;
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing(prev => !prev);
-
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible(prev => (prev === 8 ? 1 : prev + 1));
   const boxVariants = {
-    initial: { opacity: 0, scale: 0 },
-    visible: { opacity: 1, scale: 1, rotateZ: 360 },
-    leaving: { opacity: 0, scale: 0, y: 50 },
+    invisible: { x: 500, opacity: 0, scale: 0 },
+    visible: { x: 0, scale: 1, opacity: 1, transition: { duration: 1 } },
+    exit: { x: -500, opacity: 0, scale: 0, rotateZ: 1800, transition: { duration: 1 } },
   };
 
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
       <AnimatePresence>
-        {showing ? (
-          <Box variants={boxVariants} initial="initial" animate="visible" exit="leaving">
-            adsfads
-          </Box>
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8].map(i =>
+          i === visible ? (
+            <Box variants={boxVariants} initial="invisible" animate="visible" exit="exit" key={i}>
+              {i}
+            </Box>
+          ) : null,
+        )}
       </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
     </Wrapper>
   );
 }
